25e7f816c30892c17f4f2e3f8c26480b7b721dd1
Vít Kolář <kolavitek@gmail.com>
2021-02-27 18:33:04 +0100

    Database model and connections

---
+++ CarServis/CarServis/DAL/DatabaseContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarServis.DAL
+{
+    class DatabaseContext : DbContext
+    {
+        public DatabaseContext(): base("ServisOrders"){
+
+        }
+        public DbSet<Order> Order { get; set; }
+    }
+}
---
+++ CarServis/CarServis/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarServis
+{
+    class Order
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Surname { get; set; }
+        public String PhoneNumber { get; set; }
+        public String Email { get; set; }
+        public String Description { get; set; }
+        public DateTime OrderDateTime { get; set; }
+    }
+}
