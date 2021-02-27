9cadab39a3ab795384ca93f0deda82e9237e22ac
Vít Kolář <kolavitek@gmail.com>
2021-02-27 19:57:46 +0100

    DatabaseContext fixed

--- CarServis/CarServis/DAL/DatabaseContext.cs
+++ CarServis/CarServis/DAL/DatabaseContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
@@ -7,6 +8,7 @@ using System.Threading.Tasks;
 
 namespace CarService.DAL
 {
+    [DbConfigurationType(typeof(MySqlEFConfiguration))]
     class DatabaseContext : DbContext
     {
         public DatabaseContext(): base("ServiceOrders"){
